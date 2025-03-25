1b6c5ac4d2d1ea7ef84c24ff1443ca8b9e21bd95
rumaisaliaqat <rumaisaliaqat786@gmail.com>
2025-03-25 21:26:17 +0500

    Update Task 01.cpp

--- Task 01.cpp
+++ Task 01.cpp
@@ -7,6 +7,22 @@ class List {
 	int maxSize;
 	int currentSize;
 public:
+	  List(int size = 10) : maxSize(size), currentSize(0) {
+       arr = new Type[maxSize];
+   }
+
+   List(const List& other) {
+       maxSize = other.maxSize;
+       currentSize = other.currentSize;
+       arr = new Type[maxSize];
+       for (int i = 0; i < currentSize; i++) {
+           arr[i] = other.arr[i];
+       }
+   }
+
+   virtual ~List() {
+       delete[] arr;
+   }
 	virtual void addElementAtFirstIndex(Type) = 0;
 	virtual void addElementAtLastIndex(Type) = 0;
 	virtual void removeElementFromEnd() = 0;
